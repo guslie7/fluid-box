99a6a3ed7c5d91c3d401dfdf570441823eac71e0
guslie7 <gugsliesenfeld@gmail.com>
2023-12-26 21:45:43 -0300

    feat: expandindo os elementos quando a pagina carrega

--- main.js
+++ main.js
@@ -34,20 +34,6 @@
     const transformStr = `translateX(${-target}px)`
     container.classList.add('marquee-transition');
     container.style.transform = transformStr;
-
-    // const interval = setInterval(() => {
-    //   counter -= 50;
-    //   const transformStr = `translateX(${counter}px)`
-    //   container.style.transform = transformStr;
-
-    //   console.log(container);
-
-    //   if (Math.abs(counter) >= target) {
-    //     clearInterval(interval);
-    //   }
-    //   console.log(transformStr);
-    //   console.log(counter);
-    // }, 200);
   }
 
   /**
@@ -61,25 +47,70 @@
     container.style.transform = transformStr;
   }
 
-  const elements = document.querySelectorAll('.box-fluid');
-  elements.forEach((element) => {
-    const mode = element.getAttribute('data-mode');
-    if (mode === 'marquee') {
+  function addListeners() {
+    const elements = document.querySelectorAll('.box-fluid');
+    elements.forEach((element) => {
+      const mode = element.getAttribute('data-mode');
+      if (mode === 'marquee') {
 
-      element.addEventListener('mouseenter', () => {
-        marqueeContent(element);
-      });
+        element.addEventListener('mouseenter', () => {
+          marqueeContent(element);
+        });
 
-      element.addEventListener('mouseleave', () => {
-        removeMarquee(element);
-      })
-    } else {
-      element.addEventListener('mouseenter', () => {
-        if (shouldExpand(element)) {
-          const newHeight = expandContent(element);
-          console.log(`Nova altura para ccomputador ${newHeight}px`);
-        }
-      })
+        element.addEventListener('mouseleave', () => {
+          removeMarquee(element);
+        })
+      } else {
+        element.addEventListener('mouseenter', () => {
+          if (shouldExpand(element)) {
+            const newHeight = expandContent(element);
+            console.log(`Nova altura para computar ${newHeight}px`);
+          }
+        })
+      }
+    })
+  }
+
+  function openAllElements(limit = 1) {
+    const elements = document.querySelectorAll('.box-fluid');
+    const targetlimit = Math.min(limit, elements.length);
+    let counter = 0;
+    function expandNextElement() {
+      if (counter === targetlimit) {
+        return;
+      }
+
+      const currentElement = elements[counter];
+      expandContent(currentElement);
+      counter++;
+      window.requestAnimationFrame(expandNextElement);
     }
-  })
+    window.requestAnimationFrame(expandNextElement);
+  }
+
+  function shouldOpenElements() {
+    const container = document.querySelector('.box-container');
+    const autoExpand = container.getAttribute('data-auto-expand');
+    const quantity = parseInt(container.getAttribute('data-expand-quantity'));
+
+    if (quantity < 0) {
+      throw Error('quantidade deve ser maior que 0!')
+    }
+
+    if (isNaN(quantity)) {
+      throw Error('quantidade deve ser um número')
+    }
+
+    if (autoExpand === 'true') {
+      openAllElements(quantity);
+    }
+    
+    if ( autoExpand !== 'true' && autoExpand !== 'false'  ) {
+      console.warn('data-auto-expand foi fornecido, mas é diferente de true/false')
+    }
+
+  }
+
+  addListeners();
+  shouldOpenElements();
 })()
